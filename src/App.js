6ee0e06d2c66f3fa0c84f39df83c7fdfc34efb40
Philippe Villette <iphone6060@gmail.com>
2019-11-08 11:32:15 +0100

    fonctionnement de base ok

--- src/App.js
+++ src/App.js
@@ -1,26 +1,103 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Task from "./composants/Task";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends Component {
+  // mon state contient un tableau représentant mes taches
+  state = {
+    tasks: [
+      {
+        name: "Toto",
+        done: false
+      },
+      {
+        name: "Tata",
+        done: false
+      }
+    ],
+    newTask: ""
+  };
+
+  // retourne l'index de la tache name dans le tableau de l'état
+  getIndex(name) {
+    return this.state.tasks.indexOf(name);
+  }
+
+  // supprime une tache
+  delete(name) {
+    const index = this.getIndex(name);
+    const temp = this.state.tasks.slice();
+    temp.splice(index, 1);
+    this.setState({ tasks: temp });
+  }
+
+  // permute l'état (fait / non fait) d'une tache
+  toggle(name) {
+    const index = this.getIndex(name);
+    const temp = this.state.tasks.slice();
+    temp[index].done = !temp[index].done;
+    this.setState({ tasks: temp });
+  }
+
+  // ajoute une tache dans la liste
+  add(name) {
+    const task = { name: name, done: false };
+    const temp = this.state.tasks.slice();
+    temp.push(task);
+    // on met a jour le state avec le nouveau tableau contenant la tache supplémentaire et on vide la newTask pour que l'input soit vidé
+    this.setState({ tasks: temp, newTask: "" });
+  }
+
+  render() {
+    const tabTasks = this.state.tasks.slice();
+    // tri du tableau pour afficher les taches "done" en dernier
+    tabTasks.sort((a, b) => {
+      if (a.done) {
+        return 1;
+      } else {
+        return -1;
+      }
+    });
+
+    return (
+      <div className="app">
+        <h1>To-Do List</h1>
+        {tabTasks.map((value, index) => {
+          return (
+            <Task
+              key={index}
+              name={value.name}
+              done={value.done}
+              onDelete={() => {
+                this.delete(value);
+              }}
+              onToggle={() => {
+                this.toggle(value);
+              }}
+            />
+          );
+        })}
+        <input
+          className="input-task"
+          placeholder="Titre"
+          type="text"
+          name="add"
+          value={this.state.newTask}
+          onChange={event => {
+            this.setState({ newTask: event.target.value });
+          }}
+        />
+        <button
+          className={this.state.newTask === "" ? "button-add disabled" : "button-add"}
+          onClick={() => {
+            this.add(this.state.newTask);
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          Ajouter une tâche
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/composants/Task.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Task.css";
+
+// Composant représentant une ligne "tache" contenant le bouton pour la supprimer et son texte
+const Task = props => {
+  return (
+    <div className="task">
+      <div className="button-delete" onClick={props.onDelete}>
+        X
+      </div>
+      <h2 className={props.done ? "done" : ""} onClick={props.onToggle}>
+        {props.name}
+      </h2>
+    </div>
+  );
+};
+
+export default Task;
